9b50bbee7f48891a81118732424e02f0da92b44d
MerterosBehind <429638138@qq.com>
2022-11-21 17:30:58 +0800

    添加窗帘信息查询页面

--- ManagementSystem/src/main/java/com/curtain/managementsystem/controller/CurtainController.java
+++ ManagementSystem/src/main/java/com/curtain/managementsystem/controller/CurtainController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -26,5 +27,14 @@ public class CurtainController {
     public List<Curtain> getCurtainInfo(){
         List<Curtain> curtainList = curtainService.getCurtainList();
         return curtainList;
+    }
+    @GetMapping("/add")
+    @ResponseBody
+    public HashMap<String,Object> addCurtainInfo(){
+        System.out.println("新增接口调用！");
+        HashMap<String,Object> resultMap = new HashMap<>();
+        resultMap.put("state",200);
+        resultMap.put("result","窗帘新增成功！");
+        return resultMap;
     }
 }
