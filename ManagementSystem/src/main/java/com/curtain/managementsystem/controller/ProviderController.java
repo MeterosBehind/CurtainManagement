9775f0596a8a8922ce70ac1bd0cce2d741325560
MerterosBehind <429638138@qq.com>
2022-12-06 14:48:54 +0800

    前端：添加表格属性

--- ManagementSystem/src/main/java/com/curtain/managementsystem/controller/ProviderController.java
+++ ManagementSystem/src/main/java/com/curtain/managementsystem/controller/ProviderController.java
@@ -1,10 +1,11 @@
 package com.curtain.managementsystem.controller;
 
-import javax.jms.DeliveryMode;
-import javax.jms.Queue;
+import javax.jms.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,19 +28,75 @@ public class ProviderController {
         jmsMessagingTemplate.convertAndSend(queue, name);
         //方法二：这种方式不需要手动创建queue，系统会自行创建名为test的队列
         //jmsMessagingTemplate.convertAndSend("test", name);
-        for(int i=0;i<1000;i++){
+        for(int i=1;i<=1000;i++){
             long curTime = System.currentTimeMillis();
             jmsMessagingTemplate.convertAndSend(queue, "msg_"+i+"_"+curTime);
         }
         JmsTemplate jmsTemplate = jmsMessagingTemplate.getJmsTemplate();
         jmsTemplate.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
         jmsTemplate.setExplicitQosEnabled(true);
+        jmsTemplate.setSessionTransacted(true);
         jmsMessagingTemplate.setJmsTemplate(jmsTemplate);
-        System.out.println("当前发送消息模式："+jmsMessagingTemplate.getJmsTemplate().getDeliveryMode());
+        System.out.println("当前发送消息模式(持久性/非持久性)："+jmsMessagingTemplate.getJmsTemplate().getDeliveryMode());
+        System.out.println("当前发送消息模式(事务/非事务)："+jmsMessagingTemplate.getJmsTemplate().getSessionAcknowledgeMode());
         for(int i=1000;i<2000;i++){
-            long curTime = System.currentTimeMillis();
-            jmsMessagingTemplate.convertAndSend(queue, "msg_"+i+"_"+curTime);
+            jmsMessagingTemplate.convertAndSend(queue, "msg_"+i);
         }
         System.out.println("发送ActiveMQ消息总耗时："+(System.currentTimeMillis()-oldTime)+"毫秒");
     }
+
+
+    @RequestMapping("sendNormal")
+    public void sendNormal(String name) {
+        long oldTime = System.currentTimeMillis();
+        for(int i=1;i<=100000;i++){
+            jmsMessagingTemplate.convertAndSend(queue, "msg_"+i);
+        }
+        System.out.println("发送ActiveMQ消息总耗时："+(System.currentTimeMillis()-oldTime)+"毫秒");
+    }
+
+    @RequestMapping("sendNormal4ZK")
+    public void sendNormal4ZK(String name) {
+        long oldTime = System.currentTimeMillis();
+        for(int i=1;i<=50000;i++){
+            jmsMessagingTemplate.convertAndSend(queue, "msg_"+i);
+        }
+        System.out.println("发送ActiveMQ消息总耗时："+(System.currentTimeMillis()-oldTime)+"毫秒");
+    }
+
+    @RequestMapping("sendBusiness")
+    @Transactional
+    public void sendNew(String name) {
+        //获取连接工厂
+        ConnectionFactory connectionFactory = jmsMessagingTemplate.getConnectionFactory();
+        Session session = null;
+        try{
+            long oldTime = System.currentTimeMillis();
+            //创建连接
+            Connection connection = connectionFactory.createConnection();
+            //参数一：是否开启消息事务
+            session = connection.createSession(true,Session.AUTO_ACKNOWLEDGE);
+            //创建生产者
+            MessageProducer producer = session.createProducer(queue);
+
+            for(int i=1;i<=100000;i++){
+                TextMessage textMessage = session.createTextMessage("msg_"+i);
+                producer.send(textMessage);
+            }
+            //注意：一旦开启事务发送，那么就必须使用commit方法进行事务提交，否则消息无法到达MQ服务器
+            session.commit();
+            session.close();
+            System.out.println("发送10W的ActiveMQ事务消息总耗时："+(System.currentTimeMillis()-oldTime)+"毫秒");
+        }catch(JMSException e){
+            e.printStackTrace();
+            //消息事务回滚
+            try{
+                session.rollback();
+            }catch(JMSException e1){
+                e1.printStackTrace();
+            }
+
+        }
+    }
+
 }
--- ManagementSystem/src/main/java/com/curtain/managementsystem/service/ConsumerService.java
+++ ManagementSystem/src/main/java/com/curtain/managementsystem/service/ConsumerService.java
@@ -13,13 +13,25 @@ public class ConsumerService {
     private JmsMessagingTemplate jmsMessagingTemplate;
 
     // 使用JmsListener配置消费者监听的队列，其中name是接收到的消息
-    @JmsListener(destination = "ActiveMQQueue")
+//    @JmsListener(destination = "ActiveMQQueue")
     // SendTo 会将此方法返回的数据, 写入到 OutQueue 中去.
     @SendTo("SQueue")
     public String handleMessage(String name) throws Exception {
-        System.out.println("接受开始----:" + name);
-        Thread.sleep(500);
-        System.out.println("成功接受Name:" + name);
+
+        long oldTime = System.currentTimeMillis();
+        if("msg_1".equals(name)){
+            System.out.println("开始时间："+System.currentTimeMillis());
+        } else if ("msg_50000".equals(name)) {
+            System.out.println("结束时间："+System.currentTimeMillis());
+        }
+//        System.out.println("每次耗时："+(System.currentTimeMillis()-oldTime)+"毫秒");
+//        System.out.println("成功接受数据，线程：" +Thread.currentThread().getName() +"，名称："+ name);
         return "成功接受Name:" + name;
+    }
+
+//    @JmsListener(destination = "ActiveMQQueue")
+    @SendTo("SQueue1")
+    public String handelMessage(String name){
+        return "2----成功接受Name:" + name;
     }
 }
