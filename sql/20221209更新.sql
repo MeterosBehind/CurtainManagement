f889cb0e76f3531a4549ccb8a72a8d47a2ccc5d4
MerterosBehind <429638138@qq.com>
2022-12-09 10:32:39 +0800

    sql更新

---
+++ sql/20221209更新.sql
@@ -0,0 +1,3 @@
+ALTER TABLE curtain_info MODIFY COLUMN market_time DATETIME;
+ALTER TABLE curtain_info MODIFY COLUMN created_time DATETIME;
+ALTER TABLE curtain_info MODIFY COLUMN last_updated_time DATETIME;
